5d390980eb4a0a59d454f34f968a6a2f1f07a55a
Roman Valls Guimera <brainstorm@nopcode.org>
2019-12-12 21:26:03 +1100

    Fixup CSI bins for range function, needs tests

--- src/csi.rs
+++ src/csi.rs
@@ -1,35 +1,36 @@
-///// BinsForRange returns the list of bins that may overlap with the zero-based region
-///// defined by [start, end). The minShift and depth parameters control the minimum interval width
-///// and number of binning levels, respectively.
-//fn BinsForRange(start: u32, mut end: u32, minShift: u32, depth: u32) -> Vec<u8> {
-//    let mut maxWidth = maximumBinWidth(minShift, depth);
-//    let mut bins: Vec<u8> = Vec::new();
-//
-//// if end == 0 || end > maxWidth {
-////    end = maxWidth;
-//// }
-//// if(end <= start) { return n }
-////}
-////
-////if start > maxWidth { return None }
-//
-//// This is derived from the C examples in the CSI index specification.
-//    end = end - 1;
-//
-//    for(l, t, s = uint(0), uint(0), uint(minShift+depth*3); l <= uint(depth); l++ {
-//        let b = t + (start >> s);
-//        let e = t + (end >> s);
-//
-//        for i in b; i <= e; {
-//            if (i <= e) bins.append(bins);
-//        }
-//
-//        s -= 3;
-//        t += 1 << (l * 3);
-//    }
-//    return bins;
-//}
-//
-//fn maximumBinWidth(minShift: u32, depth: u32) -> u32 {
-//    return 1 << minShift + depth * 3;
-//}
+/// BinsForRange returns the list of bins that may overlap with the zero-based region
+/// defined by [start, end). The minShift and depth parameters control the minimum interval width
+/// and number of binning levels, respectively.
+fn bins_for_range(start: u32, mut end: u32, min_shift: u32, depth: u32) -> Vec<u32> {
+    let max_width = maximum_bin_width(min_shift, depth);
+    let mut bins: Vec<u32> = Vec::new();
+
+    if end == 0 || end > max_width { end = max_width; }
+//    if end <= start { return None }
+//    if start > maxWidth { return None }
+
+    // This is derived from the C examples in the CSI index specification.
+    end -= 1;
+    let mut l = 0;
+    let mut t = 0;
+    let mut s = min_shift + depth * 3;
+
+     while l <= depth {
+        let b = t + (start >> s);
+        let e = t + (end >> s);
+        let i = b;
+
+        while i <= e {
+            bins.push(i)
+        }
+
+        s -= 3;
+        t += 1 << (l * 3);
+        l += 1;
+    }
+    return bins;
+}
+
+fn maximum_bin_width(min_shift: u32, depth: u32) -> u32 {
+    return 1 << min_shift + depth * 3;
+}
