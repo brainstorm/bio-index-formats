ac392d0fb8f28621ecebc0c3775d1a4c51921719
Roman Valls Guimera <brainstorm@nopcode.org>
2020-01-16 15:44:47 +1100

    reg2bins oxidized, next up: use bins list to efficiently parse chunks. Might explore lookup tables in the not-so-distant future: https://www.youtube.com/watch?v=LnqqvVp_UZg

--- src/csi.rs
+++ src/csi.rs
@@ -1,13 +1,14 @@
 /// BinsForRange returns the list of bins that may overlap with the zero-based region
 /// defined by [start, end). The minShift and depth parameters control the minimum interval width
 /// and number of binning levels, respectively.
+// This is the generalized CSI implementation from Google's htsget
 pub fn bins_for_range(start: u32, mut end: u32, min_shift: u32, depth: u32) -> Vec<u32> {
     let max_width = maximum_bin_width(min_shift, depth);
     let mut bins: Vec<u32> = Vec::new();
 
     if end == 0 || end > max_width { end = max_width; }
-//    if end <= start { return None }
-//    if start > maxWidth { return None }
+//    if end <= start { return Option(None) }
+//    if start > maxWidth { return Option(None) }
 
     // This is derived from the C examples in the CSI index specification.
     end -= 1;
@@ -36,12 +37,45 @@ fn maximum_bin_width(min_shift: u32, depth: u32) -> u32 {
 }
 
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
+// This is the SAM spec C code, oxidized by hand.
+// See: https://gist.github.com/brainstorm/f76da194a03730a98e23766134d54d7b
+pub fn reg2bins(beg: u64, mut end: u64) -> Vec<u64> {
+    let mut i: u64 = 0;
+    let mut k: u64;
+    end -= 1;
+    i = i + 1;
+    let mut list = Vec::<u64>::new();
+
+    k = 1 + (beg >> 26);
+    while k <= 1 + (end >> 26) { i += 1; list.push(k); k += 1 }
+
+    k = 9 + (beg >> 23);
+    while k <= 9 + (end >> 23) { i += 1; list.push(k); k += 1 }
+
+    k = 73 + (beg >> 20);
+    while k <= 73 + (end >> 20) { i += 1; list.push(k); k += 1 }
+
+    k = 585 + (beg >> 17);
+    while k <= 585 + (end >> 17) { i += 1; list.push(k); k += 1 }
+
+    k = 4681 + (beg >> 14);
+    while k <= 4681 + (end >> 14) { i += 1; list.push(k); k += 1 }
+
+    return list;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::csi::reg2bins;
+
+    #[test]
+    fn csi_test() {
+        reg2bins(3, 10);
+    }
+
 //
 //    #[test]
 //    fn csi_test() {
 //        bins_for_range(3, 10,14, 5);
 //    }
-//}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-use bio_index_formats::parser_bai::{parse_bai, parse_voffsets, VirtualOffset};
+use bio_index_formats::parser_bai::{parse_bai, parse_voffsets, Ref};
+use bio_index_formats::csi::reg2bins;
+
+//fn htsget_query(refs: &Vec<Ref>, chrom: u32, start: u64, end: u64) -> (u64, u64) {
+//    let mut range_beg = u64::max_value;
+//    let mut range_end: u64 = 0;
+//
+//    for reference in refs.iter() {
+//        // XXX: Check that this reference corresponds to the chrom we are looking for
+//        // Read BAM header to retrieve chrom names (code in htsget-aws from rust-htslib)
+//        for bin in reg2bins(start, end).iter() {
+//            for chunk in bin.chunks.iter() {
+//                range_beg = range_beg.min(chunk.chunk_beg);
+//                range_end = range_end.max(chunk.chunk_beg);
+//            }
+//        }
+//    }
+//
+//    (range_beg, range_end)
+//}
 
 fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
     const BAI_FILE: &'static [u8] = include_bytes!("../tests/data/htsnexus_test_NA12878.bam.bai");
     //const BAI_FILE: &'static [u8] = include_bytes!("../tests/data/SBJ00154_PRJ190634_LPRJ190634-ready.bam.bai");
 
     let bai = parse_bai(BAI_FILE);
-    let a_chunk = bai.map(|r| r.1.refs)
-                               .map(|b| b[10].clone().bins)
-                               .map(|c| c[1].clone().chunks)
-                               .map(|cp| cp[0].clone())?;
+    let refs = bai.map(|r| r.1.refs)?;
 
-    dbg!(parse_voffsets(a_chunk.chunk_beg));
-    dbg!(parse_voffsets(a_chunk.chunk_end));
+//    let a_bin = &a_chunk[10].bins[0].chunks;
+//    dbg!(a_bin);
+//                               .map(|b| b[10].bins)
+//                               .map(|c| c[1].clone().chunks)
+//                               .map(|cp| cp[0].clone())?;
+
+//    dbg!(parse_voffsets(a_chunk.chunk_beg)?);
+//    dbg!(parse_voffsets(a_chunk.chunk_end)?);
 
     Ok(())
 }
