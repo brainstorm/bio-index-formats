b0b6e125d4904631989f7bdedb0df1333b9fa6c8
Roman Valls Guimera <brainstorm@nopcode.org>
2020-01-19 22:18:34 +1100

    First compressed bytes offsets returned, cleaning up

--- src/csi.rs
+++ src/csi.rs
@@ -1,42 +1,3 @@
-/// BinsForRange returns the list of bins that may overlap with the zero-based region
-/// defined by [start, end). The minShift and depth parameters control the minimum interval width
-/// and number of binning levels, respectively.
-// This is the generalized CSI implementation from Google's htsget
-pub fn bins_for_range(start: u32, mut end: u32, min_shift: u32, depth: u32) -> Vec<u32> {
-    let max_width = maximum_bin_width(min_shift, depth);
-    let mut bins: Vec<u32> = Vec::new();
-
-    if end == 0 || end > max_width { end = max_width; }
-//    if end <= start { return Option(None) }
-//    if start > maxWidth { return Option(None) }
-
-    // This is derived from the C examples in the CSI index specification.
-    end -= 1;
-    let mut l = 0;
-    let mut t = 0;
-    let mut s = min_shift + depth * 3;
-
-     while l <= depth {
-        let b = t + (start >> s);
-        let e = t + (end >> s);
-        let i = b;
-
-        while i <= e {
-            bins.push(i)
-        }
-
-        s -= 3;
-        t += 1 << (l * 3);
-        l += 1;
-    }
-    return bins;
-}
-
-fn maximum_bin_width(min_shift: u32, depth: u32) -> u32 {
-    return 1 << min_shift + depth * 3;
-}
-
-
 // This is the SAM spec C code, oxidized by hand.
 // See: https://gist.github.com/brainstorm/f76da194a03730a98e23766134d54d7b
 pub fn reg2bins(beg: u32, mut end: u32) -> Vec<u32> {
@@ -44,28 +5,34 @@ pub fn reg2bins(beg: u32, mut end: u32) -> Vec<u32> {
     end -= 1;
 
     let mut list = Vec::<u32>::new();
-    list.push(0); // 1-indexed
 
     k = 1 + (beg >> 26);
     while k <= 1 + (end >> 26) { list.push(k); k += 1 }
-
     k = 9 + (beg >> 23);
     while k <= 9 + (end >> 23) { list.push(k); k += 1 }
-
     k = 73 + (beg >> 20);
     while k <= 73 + (end >> 20) { list.push(k); k += 1 }
-
     k = 585 + (beg >> 17);
     while k <= 585 + (end >> 17) { list.push(k); k += 1 }
-
     k = 4681 + (beg >> 14);
     while k <= 4681 + (end >> 14) { list.push(k); k += 1 }
 
     return list;
 }
 
-//pub fn region_contains_bin(reference_id: u32, bin_id: u32, bins: Vec<u32>) {
-//}
+// Instead of returning a list with the R-Tree path traversal, returns
+// the bin we are interested in directly
+pub fn reg2bin(beg: u32, mut end: u32) -> u32
+{
+    end -= 1;
+    if beg>>14 == end>>14 { return ((1<<15)-1)/7 + (beg>>14) }
+    if beg>>17 == end>>17 { return ((1<<12)-1)/7 + (beg>>17) }
+    if beg>>20 == end>>20 { return ((1<<9)-1)/7 + (beg>>20) }
+    if beg>>23 == end>>23 { return ((1<<6)-1)/7 + (beg>>23) }
+    if beg>>26 == end>>26 { return ((1<<3)-1)/7 + (beg>>26) }
+    
+    return 0;
+}
 
 #[cfg(test)]
 mod tests {
@@ -86,12 +53,5 @@ mod tests {
 //        let res = reg2bins(-1, 0);
 //        let expected = [4680];
 //        assert_eq!(res, expected);
-//    }
-
-
-//
-//    #[test]
-//    fn csi_test() {
-//        bins_for_range(3, 10,14, 5);
 //    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::path::Path;
-//use std::collections::HashSet;
 
 use bio_index_formats::parser_bai::{parse_bai, parse_voffset, Ref};
-use bio_index_formats::csi::reg2bins;
+use bio_index_formats::csi::{ reg2bin };
 use rust_htslib::bam::{ Reader, Read };
 
 pub fn reference_ids(fname: String) -> Vec<String> {
@@ -16,18 +15,16 @@ pub fn reference_ids(fname: String) -> Vec<String> {
 fn htsget_query(reference: &Ref, start: u32, end: u32) -> (u32, u32) {
     let mut range_beg = u32::max_value(); // Must be Option instead of Integer... if it does not find anything, then None.
     let mut range_end: u32 = 0;
-    let bin_ids:Vec<u32>;
+    let bin_id: u32;
 
-//    let bin_ids: HashSet::<u32> = reg2bins(start, end).iter().cloned().collect();
-    bin_ids = reg2bins(start, end);
-    println!("{:?}", &bin_ids);
+    bin_id = reg2bin(start, end);
 
-    //println!("{:?}", reference);
     for bin in reference.bins.iter() {
-        if bin_ids.contains(&bin.bin_id) { // XXX: Explore sets instead. contains is not efficient
+        if bin_id == bin.bin_id {
             for chunk in bin.chunks.iter() {
-                let chunk_beg = parse_voffset(chunk.chunk_beg).1;
-                let chunk_end = parse_voffset(chunk.chunk_end).1;
+                let chunk_beg = parse_voffset(chunk.chunk_beg).0;
+                let chunk_end = parse_voffset(chunk.chunk_end).0;
+
                 range_beg = range_beg.min(chunk_beg);
                 range_end = range_end.max(chunk_end);
             }
@@ -47,32 +44,17 @@ fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
 
     let bam_fname = "tests/data/htsnexus_test_NA12878.bam";
     let ref_names = reference_ids(bam_fname.to_string());
-    //println!("{:?}", ref_names);
-    //dbg!(ref_names);
 
     let bai = parse_bai(BAI_FILE);
     let refs = bai.map(|r| r.1.refs)?;
-    //println!("References = {:?}", refs);
-    //println!("Number of refs in the BAI = {}", refs.len());
 
     let chrom = "11";
 
     let ref_id = ref_names.iter().position(|name| name == chrom).unwrap();
-    //println!("ref_id = {}", ref_id);
     let reference = &refs[ref_id];
-    println!("reference = {:?}", reference);
     
     let range = htsget_query(reference, 4999976, 5002147);
     dbg!(range);
-
-//    let a_bin = &a_chunk[10].bins[0].chunks;
-//    dbg!(a_bin);
-//                               .map(|b| b[10].bins)
-//                               .map(|c| c[1].clone().chunks)
-//                               .map(|cp| cp[0].clone())?;
-
-//    dbg!(parse_voffsets(a_chunk.chunk_beg)?);
-//    dbg!(parse_voffsets(a_chunk.chunk_end)?);
 
     Ok(())
 }
