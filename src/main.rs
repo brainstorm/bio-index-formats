a318b5a5e7bdb4a10717f57fdb703cf11b13d92d
Roman Valls Guimera <brainstorm@nopcode.org>
2021-04-05 21:41:13 +1000

    Adjust path to the location of generated BAM files

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bio_index_formats::parser_bai::parse_bai;
 use bio_index_formats::generate_bam::generate_bam;
 
 fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
-    const BAI_FILE: &'static [u8] = include_bytes!("../temp_mapped.bam.bai");
+    const BAI_FILE: &'static [u8] = include_bytes!("../tests/data/generated/temp_mapped.bam.bai");
     dbg!(parse_bai(BAI_FILE));
     //generate_bam();
     Ok(())
